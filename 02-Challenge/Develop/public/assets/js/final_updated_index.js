f73505c16956641032af2bfdd0cfe2c001f81af5
keybit-1 <131838974+keybit-1@users.noreply.github.com>
2023-08-17 16:56:19 -0500

    completed front end
    for now...

---
+++ 02-Challenge/Develop/public/assets/js/final_updated_index.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const BASE_URL = 'http://localhost:3000';
+    
+    const noteTitle = document.querySelector('.note-title');
+    const noteText = document.querySelector('.note-textarea');
+    const saveNoteBtn = document.querySelector('#save-note');
+    const newNoteBtn = document.querySelector('.new-note');
+    const noteList = document.querySelector('.list-container .list-group');
+
+    const fetchNotes = async () => {
+        try {
+            let response = await fetch(`${BASE_URL}/api/notes`);
+            return await response.json();
+        } catch (error) {
+            console.error("Error fetching notes:", error);
+        }
+    };
+
+    const postNote = async (note) => {
+        try {
+            await fetch(`${BASE_URL}/api/notes`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(note)
+            });
+        } catch (error) {
+            console.error("Error saving note:", error);
+        }
+    };
+
+    const deleteNoteById = async (id) => {
+        try {
+            await fetch(`${BASE_URL}/api/notes/${id}`, {
+                method: 'DELETE'
+            });
+        } catch (error) {
+            console.error("Error deleting note:", error);
+        }
+    };
+
+    const handleNoteView = (note) => {
+        noteTitle.value = note.title;
+        noteText.value = note.text;
+    };
+
+    const renderNotes = async () => {
+        const notes = await fetchNotes();
+        noteList.innerHTML = ''; // clear the note list
+
+        notes.forEach(note => {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.setAttribute('data-note', JSON.stringify(note));
+            
+            const noteTitleSpan = document.createElement('span');
+            noteTitleSpan.textContent = note.title;
+            li.appendChild(noteTitleSpan);
+            
+            const deleteBtn = document.createElement('span');
+            deleteBtn.classList.add('delete-note');
+            deleteBtn.textContent = ' X';
+            deleteBtn.addEventListener('click', async (e) => {
+                e.stopPropagation();  // Prevent triggering the li's click event
+                await deleteNoteById(note.id);
+                renderNotes();
+            });
+
+            li.appendChild(deleteBtn);
+
+            // Making the entire list item clickable
+            li.addEventListener('click', () => handleNoteView(note));
+
+            noteList.appendChild(li);
+        });
+    };
+
+    saveNoteBtn.addEventListener('click', async () => {
+        const title = noteTitle.value.trim();
+        const text = noteText.value.trim();
+
+        if (title && text) {
+            await postNote({ title, text });
+            noteTitle.value = '';
+            noteText.value = '';
+            renderNotes();
+        }
+    });
+
+    newNoteBtn.addEventListener('click', () => {
+        noteTitle.value = '';
+        noteText.value = '';
+    });
+
+    renderNotes();
+});
+
+
+
+
+
+
+
+
--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');  // Importing the CORS middleware
 
 const app = express();
 
+app.use(cors());  // Using CORS middleware to handle cross-origin requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -14,24 +16,41 @@ app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 });
 
+
 app.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
-    res.json(notes);
+    try {
+        const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+        res.json(notes);
+    } catch (error) {
+        console.error('Error reading notes:', error);
+        res.status(500).send('Internal server error.');
+    }
 });
 
 app.post('/api/notes', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
-    const newNote = { ...req.body, id: Date.now().toString() };
-    notes.push(newNote);
-    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
-    res.json(newNote);
+    try {
+        let notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+        const newNote = { ...req.body, id: Date.now().toString() };
+        notes.push(newNote);
+        fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
+        res.json(newNote);
+    } catch (error) {
+        console.error('Error saving note:', error);
+        res.status(500).send('Internal server error.');
+    }
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
-    notes = notes.filter(note => note.id !== req.params.id);
-    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
-    res.sendStatus(200);
+    try {
+        let notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+        notes = notes.filter(note => note.id !== req.params.id);
+        fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error deleting note:', error);
+        res.status(500).send('Internal server error.');
+    }
 });
+
 
 
